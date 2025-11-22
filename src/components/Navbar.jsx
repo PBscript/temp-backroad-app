c687734ed5bf8cf9cda355c72c4601a491439bf8
unknown <pavel.budnik1@gmail.com>
2025-11-22 05:48:51 +0100

    Fix rel for all external links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
                 <a
                   href={link.href}
                   className="nav-link"
+                  target="_blank"
                   rel="noopener noreferrer"
                 >
                   {link.text}
@@ -34,7 +35,12 @@ const Navbar = () => {
             const { id, href, icon } = link;
             return (
               <li key={id}>
-                <a href={href} target="_blank" className="nav-icon">
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="nav-icon"
+                >
                   <i className={icon}></i>
                 </a>
               </li>
